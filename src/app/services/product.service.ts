55df15d4626bb6142d62dd5aba5a0a1e0dc6fea8
JhojanCA <jhojanalexca12@gmail.com>
2023-12-05 14:19:18 -0500

    Se crea el endpoint y los servicios

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environment/environment' 
+import { Product } from '../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private myAppUrl: string;
+  private myApiUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = 'api/productos/';
+  }
+
+  getListProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`);
+  }
 }
