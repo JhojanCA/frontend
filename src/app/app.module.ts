29c4a37132ec1ce882665e73e54ac3b2f895af0d
JhojanCA <jhojanalexca12@gmail.com>
2023-12-05 14:18:45 -0500

    Se actualizan las dependencias y los modulos necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AddEditComponent } from './components/add-edit/add-edit.component';
 import { ListProductsComponent } from './components/list-products/list-products.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AddEditComponent,
-    ListProductsComponent
+    ListProductsComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
